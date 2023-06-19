02eacebfd518623e1d8687a445fdd07641db81dc
MikeIrlbeck <mirlbeck22@gmail.com>
2023-06-19 07:19:13 -0600

    added z-20 to be the same as its containing modal

--- src/components/siteDiary/material/UnitsDropDown.tsx
+++ src/components/siteDiary/material/UnitsDropDown.tsx
@@ -27,7 +27,7 @@ const StatusDropdown = ({ value, onChange }: DropdownProps) => {
         </Select.Icon>
       </Select.Trigger>
       <Select.Portal>
-        <Select.Content className="overflow-hidden rounded-md border border-blue-300 bg-white">
+        <Select.Content className="z-20 overflow-hidden rounded-md border border-blue-300 bg-white">
           <Select.ScrollUpButton className="flex h-6 items-center justify-center bg-white text-blue-600">
             <ChevronUpIcon />
           </Select.ScrollUpButton>
