e8b70192e6e4330d1c690ab598513e15a1c61d15
MikeIrlbeck <mirlbeck22@gmail.com>
2023-06-27 20:45:27 -0600

    Zod schema gpt

--- src/server/api/routers/gpt.ts
+++ src/server/api/routers/gpt.ts
@@ -4,71 +4,11 @@ import {
   StructuredOutputParser,
 } from "langchain/output_parsers";
 import { PromptTemplate } from "langchain/prompts";
-import { z } from "zod";
+import type { z } from "zod";
 import { env } from "../../../env/server.mjs";
+import { extractInvoiceInfoSchema, gptOutputSchema } from "../../../schema/gpt";
 import { trycatch } from "../../../utils/trycatch";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-
-export const extractInvoiceInfoSchema = z.object({
-  inputText: z.string(),
-});
-
-const gptOutputSchema = z.object({
-  invoiceNo: z.string().describe("The invoice number. Defaults to ''."),
-  invoiceDate: z
-    .string()
-    .describe(
-      "The invoice date. Should be returned in the form of dd/mm/yyyy. Defaults to today's date."
-    ),
-  supplierName: z
-    .string()
-    .describe(
-      "The supplier name. The supplier name should not be mistaken as the vendor name. Defaults to ''."
-    ),
-  subtotal: z
-    .number()
-    .describe("The sum of the totalPrice of all items. Defaults to 0."),
-  taxes: z
-    .number()
-    .describe(
-      "The taxes incurred. DO NOT multiply the tax with any percentages. Defaults to 0."
-    ),
-  discount: z.number().describe("The discount given, if any. Defaults to 0."),
-  grandTotal: z
-    .number()
-    .describe("The subtotal plus taxes less discount. Defaults to 0."),
-  supplierInvoiceItems: z
-    .array(
-      z.object({
-        description: z
-          .string()
-          .describe(
-            "Item description. DO NOT include unit or quantity in the description. Defaults to ''."
-          ),
-        quantity: z
-          .number()
-          .describe(
-            "Item quantity. Should be a whole number. Treat synonymous terms like 'Quantity/ (x)' as quantity too. (x) can be any value. Defaults to 0."
-          ),
-        unit: z
-          .string()
-          .describe(
-            "Item unit. It takes values such as 'M', 'M2', 'M3', 'g', 'kg', 'tons', 'feet', 'NR', or '1'. Defaults to NR."
-          ),
-        unitPrice: z
-          .number()
-          .describe(
-            "The unit price of an item. It represents a currency or monetary value. Defaults to 0."
-          ),
-        totalPrice: z
-          .number()
-          .describe(
-            "The total price equals unitPrice multiplied by quantity. Defaults to 0."
-          ),
-      })
-    )
-    .describe("Items of the invoice. Defaults to an empty array []."),
-});
 
 export type gptOutputSchema = z.infer<typeof gptOutputSchema>;
 
